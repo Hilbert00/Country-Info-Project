b350f4cbc052ae480b3225968311572598bb9b79
Hilbert <nicolashilbert000@gmail.com>
2022-11-29 00:12:25 -0300

    1.0

--- app.js
+++ app.js
@@ -1,11 +1,79 @@
 const express = require("express");
-const app = express();
+const exphbs = require("express-handlebars");
+const numeral = require("numeral");
+
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+const app = express();
+const hbs = exphbs.create({
+    partialsDir: "./views/partials",
+});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.use(express.static("public"));
+
+app.get("/country", (req, res) => {
+    fetch(`https://restcountries.com/v3.1/alpha/${req.query.cname}`)
+        .then((response) => {
+            if (!response.ok) {
+                return Promise.reject(`${response.status}: ${response.statusText}`);
+            }
+
+            return response.json();
+        })
+        .then((json) => json[0])
+        .then((data) => {
+            const countryInfo = {
+                name: data.translations.por.common,
+                officialName: data.translations.por.official,
+                code: req.query.cname.toUpperCase(),
+                capitals: data.capital,
+
+                flag: data.flags.svg,
+
+                currency: data.currencies[Object.keys(data.currencies)[0]].name,
+                currency_initials: Object.keys(data.currencies)[0],
+                currency_symbol: data.currencies[Object.keys(data.currencies)[0]].symbol,
+
+                continent: data.region,
+                region: data.subregion,
+                latitude: data.latlng[0].toFixed(),
+                longitude: data.latlng[1].toFixed(),
+                area: numeral(data.area).format("0.000a"),
+                population: numeral(data.population).format("0.0a"),
+                borders: data.borders || "",
+
+                languages: data.languages,
+
+                timezones: String([...data.timezones]).replace(/,/g, ", "),
+            };
+
+            if (countryInfo.borders != "") {
+                countryInfo.hasBorder = true;
+                countryInfo.borders = String([...data.borders]).replace(/,/g, ", ");
+            } else {
+                countryInfo.hasBorder = false;
+            }
+
+            return countryInfo;
+        })
+        .then((result) => res.render("country", { data: result }))
+        .catch((err) => {
+            console.log(err);
+            res.redirect("/404");
+        });
+});
+
+app.get("/", (req, res) => res.render("home"));
+
+app.use((req, res, next) => {
+    res.status(404);
+
+    res.render("error");
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
 
 const html = `
 <!DOCTYPE html>
@@ -56,4 +124,4 @@ const html = `
     </section>
   </body>
 </html>
-`
+`;
