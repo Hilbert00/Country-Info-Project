4eb0cfd9240adb4165491b24bf9c930fdb596f79
Hilbert <nicolashilbert000@gmail.com>
2022-11-30 00:00:06 -0300

    final adjustments

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const numeral = require("numeral");
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 const app = express();
 const hbs = exphbs.create({
@@ -28,13 +28,13 @@ app.get("/country", (req, res) => {
                 name: data.translations.por.common,
                 officialName: data.translations.por.official,
                 code: req.query.cname.toUpperCase(),
-                capitals: data.capital,
+                capitals: data.capital ? String([...data.capital]).replace(/,/g, ", ") : "Sem capital",
 
                 flag: data.flags.svg,
 
-                currency: data.currencies[Object.keys(data.currencies)[0]].name,
-                currency_initials: Object.keys(data.currencies)[0],
-                currency_symbol: data.currencies[Object.keys(data.currencies)[0]].symbol,
+                currency: data.currencies ? data.currencies[Object.keys(data.currencies)[0]].name : null,
+                currency_initials: data.currencies ? Object.keys(data.currencies)[0] : null,
+                currency_symbol: data.currencies ? data.currencies[Object.keys(data.currencies)[0]].symbol : null,
 
                 continent: data.region,
                 region: data.subregion,
@@ -42,19 +42,12 @@ app.get("/country", (req, res) => {
                 longitude: data.latlng[1].toFixed(),
                 area: numeral(data.area).format("0.000a"),
                 population: numeral(data.population).format("0.0a"),
-                borders: data.borders || "",
+                borders: data.borders ?  String([...data.borders]).replace(/,/g, ", ") : false,
 
-                languages: data.languages,
+                languages: data.languages ? data.languages : ["Sem idioma oficial"],
 
                 timezones: String([...data.timezones]).replace(/,/g, ", "),
             };
-
-            if (countryInfo.borders != "") {
-                countryInfo.hasBorder = true;
-                countryInfo.borders = String([...data.borders]).replace(/,/g, ", ");
-            } else {
-                countryInfo.hasBorder = false;
-            }
 
             return countryInfo;
         })
@@ -74,54 +67,3 @@ app.use((req, res, next) => {
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`;
